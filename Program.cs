b257601ee004498011164422a4bd97fae3bc41ba
casio250110 <casio250110@yandex.ru>
2022-10-15 16:08:23 +0300

    Добавили третью задачу

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ int number = int.Parse(Console.ReadLine());
 Console.WriteLine($"Квадрат {number} равен {number*number}");
 Console.WriteLine("Ваше имя "+name);
 */
+/*
 int num1 = int.Parse(Console.ReadLine());
 int num2 = int.Parse(Console.ReadLine());
 if (num2*num2==num1)
@@ -20,3 +21,18 @@ if (koren == num2){
 else {
     Console.WriteLine("Нет, не является");
 }
+*/
+/* Программа будет выдавать название дня недели по заданному номеру.
+3 -> Среда
+5 -> Пятница
+*/
+Console.Write("Введите номер: ");
+int num = int.Parse(Console.ReadLine());
+if (num==1) Console.WriteLine("Понедельник");
+else if (num==2)Console.WriteLine("Вторник");
+else if (num==3)Console.WriteLine("Среда");
+else if (num==4)Console.WriteLine("Четверг");
+else if (num==5)Console.WriteLine("Пятница");
+else if (num==6)Console.WriteLine("Суббота");
+else if (num==7)Console.WriteLine("Воскресенье");
+else Console.WriteLine("Такого дня недели не существует:(");
